31b731a7dd72f3bb8f2de41d2349b29b50cf06c5
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-04-23 11:10:36 -0500

    add code to get link pages

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { allowedNodeEnvironmentFlags } = require("process");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+//Midddleware
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+app.get('/notes', (req,res) => {
+    res.sendFile(__dirname + "/public/notes.html");
+})
+
+ app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+})
+
+
+
+app.listen(3001, () => {
+    console.log (`API server now on port 3001`)
+})
