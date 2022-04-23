857a02bc422cf1876882d5ca5cec6296b597007a
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-04-23 12:04:01 -0500

    add code to get notes and save notes not working yet

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { allowedNodeEnvironmentFlags } = require("process");
+// const {} = require("process");
+const notes = require("./db/db.json")
+const path = require ('path')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 
-//Midddleware
+//Midddlewarec
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
@@ -13,13 +15,22 @@ app.use(express.static("public"));
 
 app.get('/notes', (req,res) => {
     res.sendFile(__dirname + "/public/notes.html");
+});
+
+
+app.get('/api/notes', (req,res) => {
+    res.json(notes);
+    console.log("reach")
+})
+app.post('/api/notes', (req,res) => {
+    req.body.id = notes.length.toString();
+    console.log(body)
 })
 
- app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname + "/public/index.html"));
-})
 
-
+app.get('*', (req,res) => {
+   res.sendFile(path.join(__dirname + "/public/index.html"));
+});
 
 app.listen(3001, () => {
     console.log (`API server now on port 3001`)
