65d4996178db117b698fc377ad8071c704efc7d0
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-04-24 17:54:05 -0500

    add code to try and get new message to save

--- server.js
+++ server.js
@@ -1,38 +1,61 @@
-const express = require("express");
-const fs = require("fs")
 const notes = require("./db/db.json")
-const path = require ('path')
+const express = require("express");
+const path = require('path')
+const fs = require("fs")
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 
 //Midddlewarec
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
-app.get('/notes', (req,res) => {
+// creating GET routes
+app.get('/notes', (req, res) => {
     res.sendFile(__dirname + "/public/notes.html");
 });
 
-
-app.get('/api/notes', (req,res) => {
+app.get('/api/notes', (req, res) => {
     res.json(notes);
     console.log("reach")
 });
 
-app.post('/api/notes', (req,res) => {
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, + "/db.json"));
+});
+
+app.post('/api/notes', (req, res) => {
     req.body.id = notes.length.toString();
     console.log(req.body)
 });
-
-
-app.get('*', (req,res) => {
-   res.sendFile(path.join(__dirname + "/public/index.html"));
+// creating POST routes
+app.post("/api/notes", function(req, res) {
+    fs.readFile(path.join(__dirname, + "/db.json"), "utf8", function(error, response) {
+        if (error) {
+            console.log(error);
+        }
+        const notes = JSON.parse(response);
+        const noteRequest = req.body;
+        const newNoteID = notes.length + 1;
+        const newNote = {
+            id: newNoteID,
+            title: noteRequest.title,
+            text: noteRequest.text
+        };
+        notes.push(newNote);
+        res.json(newNote);
+        fs.writeFile(path.join(__dirname, + "/db.json"), JSON.stringify(notes, null, 2), function(err) {
+            if (err) throw err;
+        });
+    });
 });
 
 app.listen(PORT, () => {
-    console.log (`API server now on port ${PORT}!`);
+    console.log(`API server now on port ${PORT}!`);
 });
