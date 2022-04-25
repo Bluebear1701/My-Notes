3f83c0c06ab560a06e12baed8b14b4d375efe171
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-04-24 20:56:49 -0500

    updated code found the redundancies

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const notes = require("./db/db.json")
 const express = require("express");
 const path = require('path')
 const fs = require("fs")
+const { v4: uuidv4 } = require('uuid');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,45 +16,49 @@ app.use(express.static("public"));
 
 // creating GET routes
 app.get('/notes', (req, res) => {
-    res.sendFile(__dirname + "/public/notes.html");
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-    console.log("reach")
-});
+// app.get('/api/notes', (req, res) => {
+//     res.json(notes);
+//     console.log("reach")
+// });
 
 app.get("/api/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, + "/db.json"));
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+    // res.json(notes);
 });
 
-function createNewNote(body, notes) {
-    const note = body;
-    notes.push(note);
-    fs.writeFileSync(
-      path.join(__dirname, "/db.json"),
-      JSON.stringify({ notes: note }, null, 2)
-    );
-    return note;
-  }
+// function createNewNote(body, notes) {
+//     const note = body;
+//     notes.push(note);
+//     fs.writeFileSync(
+//       path.join(__dirname, "./db/db.json"),
+//       JSON.stringify({ notes: note }, null, 2)
+//     );
+//     return note;
+//   }
 
 app.post("/api/notes", function (req, res) {
-    fs.readFile(path.join(__dirname, + "./db/db.json"), function (error, response) {
+    fs.readFile("./db/db.json", function (error, response) {
         if (error) {
             console.log(error);
         }
         const notes = JSON.parse(response);
         const noteRequest = req.body;
-        const newNoteID = notes.length + 1;
-        const newNote = {
-            id: newNoteID,
-            title: noteRequest.title,
-            text: noteRequest.text
-        };
-        notes.push(newNote);
-        fs.writeFile(path.join(__dirname, + "/db.json"), JSON.stringify(notes, null, 2), function (err) {
+        uuidv4();
+        console.log (noteRequest)
+
+        // const newNoteID = notes.length + 1;
+        // const newNote = {
+        //     id: newNoteID,
+        //     title: noteRequest.title,
+        //     text: noteRequest.text
+        // };
+        notes.push(noteRequest);
+        fs.writeFile("./db/db.json", JSON.stringify(notes, null, 2), function (err) {
             if (err) throw err;
-            res.json(newNote);
+            res.json(noteRequest);
         });
     });
 });
